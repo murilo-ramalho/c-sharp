dc4b3c2bc23254067c1b7c7245ebd8bc74f728c9
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-01-22 21:16:27 -0300

    Update Program.cs

--- BuckeApi/Program.cs
+++ BuckeApi/Program.cs
@@ -12,7 +12,8 @@ var s3Client = new AmazonS3Client(
     new AmazonS3Config
     {
         ServiceURL = "http://localhost:4566",
-        ForcePathStyle = true
+        ForcePathStyle = true,
+        UseHttp = true
     }
 );
 
@@ -35,4 +36,38 @@ app.MapPost("/presigned-url", ([FromForm] string fileName) =>
 })
 .DisableAntiforgery();
 
+app.MapPut("/edit-text", async (EditTextRequest request) =>
+{
+    var bucketName = "teste";
+
+    // 1. Baixar o arquivo atual
+    var getObject = await s3Client.GetObjectAsync(bucketName, request.FileName);
+
+    string currentContent;
+    using (var reader = new StreamReader(getObject.ResponseStream))
+    {
+        currentContent = await reader.ReadToEndAsync();
+    }
+
+    // 2. Editar conteúdo (exemplo: substituir tudo)
+    var updatedContent = request.NewContent;
+
+    // 3. Subir novamente (overwrite)
+    var putRequest = new PutObjectRequest
+    {
+        BucketName = bucketName,
+        Key = request.FileName,
+        ContentBody = updatedContent
+    };
+
+    await s3Client.PutObjectAsync(putRequest);
+
+    return Results.Ok(new
+    {
+        message = "Arquivo atualizado com sucesso",
+        file = request.FileName
+    });
+});
+
 app.Run("http://localhost:5000");
+record EditTextRequest(string FileName, string NewContent);
