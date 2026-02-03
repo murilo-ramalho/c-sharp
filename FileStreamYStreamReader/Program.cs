9f7f06d4f3293f2c5bdcb1218bbba5aaa54bb8ba
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-02-02 23:05:56 -0300

    tratamento de arquivos IO

---
+++ FileStreamYStreamReader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+string path = @"C:\Users\Murilo\Documents\Projects\c-sharp\FileStreamYStreamReader\archive.txt";
+FileStream fs = null;
+StreamReader sr = null;
+
+try
+{
+    sr = File.OpenText(path);
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+        System.Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error: {e}");
+}
+finally
+{
+    if (sr != null) sr.Close();
+    if (fs != null) fs.Close();
+}
+
+
+// using
+try
+{
+    using (StreamReader sr2 = File.OpenText(path))
+    {
+        while (!sr2.EndOfStream)
+        {
+            string line = sr2.ReadLine();
+            System.Console.WriteLine(line);
+        }
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error: {e}");
+}
---
+++ TratamentoArquivos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+string sourcePath = @"C:\Users\Murilo\Documents\Projects\c-sharp\TratamentoArquivos\archive.txt";
+string targetPath = @"C:\Users\Murilo\Documents\Projects\c-sharp\TratamentoArquivos\archive2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+
+    string[] lines = File.ReadAllLines(sourcePath);
+    foreach(string line in lines)
+    {
+        System.Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error: {e}");
+}
