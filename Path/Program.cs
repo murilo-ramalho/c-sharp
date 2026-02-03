10df01db178c5a2a076232339d8d4aefe38584cf
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-02-02 23:54:17 -0300

    path

---
+++ Path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+string path = @"C:\Users\Murilo\Documents\Projects\c-sharp\Path";
+
+System.Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+System.Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+System.Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+System.Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+System.Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+System.Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+System.Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+System.Console.WriteLine("GetTempPath: " + Path.GetTempPath());
