428372fe5560ca5f1c1207d0f80084b67e10c06b
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-02-02 23:33:15 -0300

    streamwriter e diretory

---
+++ Diretory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+string path = @"C:\Users\Murilo\Documents\Projects\c-sharp\EmpresaExercicio01";
+
+
+// FOLDERS
+try
+{
+    IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+    System.Console.WriteLine("FOLDERS: ");
+    foreach (string s in folders)
+    {
+        System.Console.WriteLine(s  );
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error {e}");
+}
+
+// FILES
+try
+{
+    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+    System.Console.WriteLine("FILES: ");
+    foreach (string f in files)
+    {
+        System.Console.WriteLine(f);
+    }
+
+    Directory.CreateDirectory(@"C:\Users\Murilo\Documents\Projects\c-sharp\Diretory" + "\\aaaa");
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error {e}");
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+string sourcePath = @"C:\Users\Murilo\Documents\Projects\c-sharp\StreamWriter\archive.txt";
+string targetPath = @"C:\Users\Murilo\Documents\Projects\c-sharp\StreamWriter\archive2.txt";
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using(StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine($"error: {e}");
+}
