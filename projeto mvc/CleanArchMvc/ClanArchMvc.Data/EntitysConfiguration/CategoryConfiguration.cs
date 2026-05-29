6c687a44345980381c7232cb3f161b0a944a1b83
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-05-28 21:59:06 -0300

    adicionado contexto de dados

---
+++ projeto mvc/CleanArchMvc/ClanArchMvc.Data/EntitysConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClanArchMvc.Infra.Data.EntitysConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category> 
+    {
+        public void Configure(EntityTypeBuilder<Category> builder) 
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Metais"),
+                new Category(2, "Madeiras"),
+                new Category(3, "Eletrônicos"),
+                new Category(4, "Acessorios")
+            );
+        }
+    }
+}
