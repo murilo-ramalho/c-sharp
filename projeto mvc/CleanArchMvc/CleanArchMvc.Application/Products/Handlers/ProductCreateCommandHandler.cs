ac833fd3995bc7fe02f467b2e1250c764bde485a
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-06-04 17:25:58 -0300

    adicionado handlers

---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.Application/Products/Handlers/ProductCreateCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class ProductCreateCommandHandler : IRequestHandler<ProductCreateCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductCreateCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<Product> Handle(ProductCreateCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product(request.Name, request.Description, request.Price, request.Stock, request.Image);
+
+            if (product == null)
+            {
+                throw new ArgumentNullException("Error creating entity: " + nameof(product));
+            }
+                product.CategoryId = request.CategoryID;
+
+                return await _productRepository.CreateAsync(product);
+        }
+    }
+}
---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class ProductRemoveCommandHandler : IRequestHandler<Commands.ProductRemoveCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository));
+        }
+        public async Task<Product> Handle(Commands.ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetProductByIdAsync(request.Id);
+
+            if (product == null)
+            { 
+                throw new ArgumentNullException("Entity could not be found: " + nameof(product));
+            }
+
+            return await _productRepository.DeleteAsync(product);
+        }
+    }
+}
