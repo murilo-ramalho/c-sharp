7f9e1293dd9c7ac07a8b480200d3e33a03b9f831
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-08-05 22:17:32 -0300

    adicionado identity aspnet

---
+++ projeto mvc/CleanArchMvc/ClanArchMvc.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClanArchMvc.Infra.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
