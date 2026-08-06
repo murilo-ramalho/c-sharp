1e8ec14512f821b14a0305f8b57e3b56fe5d9d8e
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-08-05 22:44:15 -0300

    modelo complexo de email

--- projeto mvc/CleanArchMvc/CleanArchMvc.Domain/Entities/Email.cs
+++ projeto mvc/CleanArchMvc/CleanArchMvc.Domain/Entities/Email.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CleanArchMvc.Domain.Entities
 {
     public class Email
     {
-        public string Intentity { get; set; }
-        public string Domain { get; set; }
+        private string Value { get; set; }
 
-        public Email(string identity, string domain)
+        private static readonly Regex EmailRegex = new(
+            @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+            RegexOptions.Compiled | 
+            RegexOptions.IgnoreCase
+        );
+
+        public Email(string endereco)
         {
-            this.Intentity = identity;
-            this.Domain = domain;
+            if (string.IsNullOrWhiteSpace(endereco))
+                throw new ArgumentNullException("Email address cannot be empty: " + nameof(endereco));
+
+            string trimmedValue = endereco.Trim().ToLowerInvariant();
+
+            if (!EmailRegex.IsMatch(trimmedValue))
+            {
+                throw new ArgumentException($"'{endereco}' is not a valid email address.");
+            }
+
+            this.Value = endereco;
         }
+
+        public override string ToString() => Value;
     }
 }
