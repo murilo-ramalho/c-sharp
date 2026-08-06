807d06172be197244dc86fb215fb2b07a88f8420
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-08-05 22:28:57 -0300

    update de pacotes, variavel email e interfaces de autenticação

---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticate(Email email, string password);
+        Task<bool> RegisterUser(Email email, string password);
+        Task Logout();
+    }
+}
---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.Domain/Entities/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Email
+    {
+        public string Intentity { get; set; }
+        public string Domain { get; set; }
+
+        public Email(string identity, string domain)
+        {
+            this.Intentity = identity;
+            this.Domain = domain;
+        }
+    }
+}
