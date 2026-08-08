c7d56d8843be0a6672a584dc106d33ff7021dadb
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-08-08 13:21:40 -0300

    adicionado roles e injeção de dependencia

--- projeto mvc/CleanArchMvc/CleanArchMvc.IoC/DependecyInjection.cs
+++ projeto mvc/CleanArchMvc/CleanArchMvc.IoC/DependecyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using ClanArchMvc.Infra.Data.Context;
+using ClanArchMvc.Infra.Data.Identity;
 using ClanArchMvc.Infra.Data.Repositories;
 using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,12 +22,23 @@ namespace CleanArchMvc.Infra.IoC
                 options.UseNpgsql(configuration.GetConnectionString("DefautConnection"
                 ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryServices>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
             services.AddAutoMapper(_ => { }, typeof(DomainToDTOMappingProfile).Assembly);
+
+            services.ConfigureApplicationCookie(opt =>
+                opt.AccessDeniedPath = "/Account/Login"); 
 
             var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(myhandlers));
