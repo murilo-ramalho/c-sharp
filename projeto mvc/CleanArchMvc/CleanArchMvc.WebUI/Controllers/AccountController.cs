b533b4d6e462d6ea3ecbb080db6030ec34476295
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-08-15 13:25:38 -0300

    adicionado views de login e registro

---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var result = await _authenticate.Authenticate(loginViewModel.Email, loginViewModel.Password);
+
+            if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (result)
+            {
+                return Redirect(loginViewModel.ReturnUrl);
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt. (password must be strong).");
+            return View(loginViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel) 
+        {
+            var result = await _authenticate.RegisterUser(registerViewModel.Email, registerViewModel.Password);
+
+            if (result)
+            {
+                return Redirect("/");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid register attempt");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authenticate.Logout();
+            return Redirect("/Account/Login");
+        }
+    }
+}
---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.WebUI/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public Email Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords don't macht")]
+        public string ConfirmPassword { get; set; }
+    }
+}
