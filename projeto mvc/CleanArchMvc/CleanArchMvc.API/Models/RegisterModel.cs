4f264ddfdacb5f360901a9ab27a97df04690f28a
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2026-09-23 08:18:19 -0300

    iniciado implementação jwt na api

---
+++ projeto mvc/CleanArchMvc/CleanArchMvc.API/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password don't macht")]
+        public string ConfirmPassword { get; set; }
+    }
+}
